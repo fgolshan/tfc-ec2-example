c812d742bbdcb731de1529149e6c0402e77008bc
lipaipai <56198699+lipaipai@users.noreply.github.com>
2020-12-05 17:01:27 +0100

    Update size argument to 1

--- main.tf
+++ main.tf
@@ -25,5 +25,6 @@ resource "aws_instance" "example" {
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-west-2a"
-  size              = 2
+  # xingong just changed the size from 2 to 1, as fari had suggested
+  size              = 1
 }
