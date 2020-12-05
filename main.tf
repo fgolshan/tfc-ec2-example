0afe2fb16360c5d438d666def68b7c97a846a998
fgolshan <75170097+fgolshan@users.noreply.github.com>
2020-12-05 15:46:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,17 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_volume_attachment" "example" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.example.id
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0157b1e4eefd91fd7"
   instance_type = "t2.micro"
+}
+
+resource "aws_ebs_volume" "example" {
+  size              = 1
 }
