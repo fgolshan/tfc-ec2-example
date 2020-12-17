a9b58526ff09c9c327a5562664c4ce0967f77ce5
lipaipai <56198699+lipaipai@users.noreply.github.com>
2020-12-17 14:35:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,20 +12,22 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_volume_attachment" "example" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.example.id
-  instance_id = aws_instance.example.id
-}
-
 resource "aws_instance" "example" {
+  // CentOS
   ami           = "ami-0157b1e4eefd91fd7"
+  // 1 CPU, 1 GB RAM
   instance_type = "t2.micro"
 }
 
 // Amazon Elastic Block Store (EBS)
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-west-2a"
-  # just changed the size from 2 to 1 and then to 4, as fari had suggested
   size              = 2
+}
+
+// Volume Attachment
+resource "aws_volume_attachment" "example" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.example.id
 }
