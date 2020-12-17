dc821e519b25982b540991c09b4afce131fc2e4d
lipaipai <56198699+lipaipai@users.noreply.github.com>
2020-12-17 16:05:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,13 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+// Volume Attachment
+resource "aws_volume_attachment" "example" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.example.id
+}
+
 resource "aws_instance" "example" {
   // CentOS
   ami           = "ami-0157b1e4eefd91fd7"
@@ -25,9 +32,4 @@ resource "aws_ebs_volume" "example" {
   size              = 2
 }
 
-// Volume Attachment
-resource "aws_volume_attachment" "example" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.example.id
-  instance_id = aws_instance.example.id
-}
+
